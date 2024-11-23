acb419bfdcd7ae990cb39f05b95062c6ef3f82ec
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-11-23 20:33:41 +0900

    ACLではなくバケットポリシーによって公開するように変更
    `apply`した際に以下のようなエラーが発生した。
    > public access control lists (ACLs) are blocked by the BlockPublicAcls block public access setting.
    
    これはデフォルトではパブリックACLが無効化された影響にかと思われる。
    https://aws.amazon.com/jp/about-aws/whats-new/2022/12/amazon-s3-automatically-enable-block-public-access-disable-access-control-lists-buckets-april-2023/
    
    `aws_s3_bucket_public_access_block`による設定が効いていない理由は不明。
    
    そもそもACLによる制御は非推奨であるのでバケットポリシーによって制御するように変更した。

--- s3_public.tf
+++ s3_public.tf
@@ -3,13 +3,6 @@ resource "aws_s3_bucket" "public" {
   bucket = "public-yuma-ito-bd-pragmatic-terraform"
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
-resource "aws_s3_bucket_acl" "public" {
-  bucket = aws_s3_bucket.public.id
-
-  acl = "public-read"
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/5.77.0/docs/resources/s3_bucket_cors_configuration
 resource "aws_s3_bucket_cors_configuration" "public" {
   bucket = aws_s3_bucket.public.id
@@ -49,4 +42,28 @@ resource "aws_s3_bucket_public_access_block" "public" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+# パブリックアクセスを許可するIAMポリシー
+data "aws_iam_policy_document" "public" {
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.public.arn}/*",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "public" {
+  bucket = aws_s3_bucket.public.id
+  policy = data.aws_iam_policy_document.public.json
+
+  # デフォルトではパブリックアクセスブロックが有効化されているので、先に無効化する必要がある
+  depends_on = [aws_s3_bucket_public_access_block.public]
 }
