8493e3efb7d6dc56c398b0a3dbd98fd2f351dfd9
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-11-23 15:30:44 +0900

    セキュリティグループの作成

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  ami                    = "ami-0c3fd0f5d33134a76"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   user_data = <<-EOF
   #!/bin/bash
@@ -10,5 +12,28 @@ resource "aws_instance" "example" {
 
   tags = {
     Name = "example"
+  }
+}
+
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "example_ec2" {
+  name = "example_ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
