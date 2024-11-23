ed9ec1de934d4f6ccb2617704c614f702eddf01f
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-11-23 15:53:42 +0900

    aws_security_groupのリソース内にingressとegressを使うのは非推奨だったので修正

--- main.tf
+++ main.tf
@@ -22,18 +22,22 @@ output "example_public_dns" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 resource "aws_security_group" "example_ec2" {
   name = "example_ec2"
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_ingress_rule
+resource "aws_vpc_security_group_ingress_rule" "example_ec2" {
+  security_group_id = aws_security_group.example_ec2.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_egress_rule
+resource "aws_vpc_security_group_egress_rule" "example_ec2" {
+  security_group_id = aws_security_group.example_ec2.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
