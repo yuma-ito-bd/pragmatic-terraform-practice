88dc1d19d0603d3d34d02ef2fc4f1c3312eb7869
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-12-07 13:41:44 +0900

    ターゲットグループとリスナールールの作成

--- alb.tf
+++ alb.tf
@@ -67,3 +67,43 @@ resource "aws_lb_listener" "pragmatic-terraform" {
     }
   }
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+resource "aws_lb_target_group" "pragmatic_terraform" {
+  name                 = "pragmatic-terraform"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.pragmatic_terraform.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+
+  depends_on = [aws_lb.pragmatic-terraform]
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener_rule
+resource "aws_lb_listener_rule" "pragmatic-terraform" {
+  listener_arn = aws_lb_listener.pragmatic-terraform.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.pragmatic_terraform.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
