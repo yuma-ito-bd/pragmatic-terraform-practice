e0d36db0ee92155623e7fb161d0a0babd71116d8
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2024-12-07 15:23:24 +0900

    ログの設定、ECSタスク実行ロールの作成

--- ecs.tf
+++ ecs.tf
@@ -15,6 +15,11 @@ resource "aws_ecs_task_definition" "pragmatic_terraform" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+
+  tags = {
+    Name = "pragmatic-terraform"
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
@@ -62,4 +67,35 @@ module "nginx_sg" {
   vpc_id    = aws_vpc.pragmatic_terraform.id
   port      = 80
   cidr_ipv4 = aws_vpc.pragmatic_terraform.cidr_block
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
+resource "aws_cloudwatch_log_group" "for_ecs" {
+  name              = "/ecs/pragmatic-terraform"
+  retention_in_days = 7
+}
+
+# ESCタスク実行ロールのポリシー
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_policy_documents = [data.aws_iam_policy.ecs_task_execution_role_policy.policy]
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
